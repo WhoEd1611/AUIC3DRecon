e7bcce3a1f8a3b4026e63b47267e20af4f6dbcc6
edrlay <edriclay2002@outlook.com>
2025-09-18 01:35:17 +1000

    added basics of skeleton

--- MergePointCloud.py
+++ MergePointCloud.py
@@ -1,3 +1,15 @@
 # Created by: Edric Lay
 # Date Created: 18/09/25
+import rclpy
+from rclpy.node import Node
+import open3d
 
+class MergePointCloud(Node):
+    def __init__(self):
+        super().__init__("point_cloud_merge_node")
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = MergePointCloud()
+	rclpy.spin(node) # This node will be kept alive independently until is killed.
+	rclpy.shutdown() 
