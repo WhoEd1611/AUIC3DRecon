a908a78c5d2e97fda122aca11803b6ee4db72a5d
WhoEd1611 <edriclay2002@outlook.com>
2025-09-21 15:07:04 +1000

    separated subscription to see if that makes it work

--- src/point_cloud_blender/src/pointCloudBlender.cpp
+++ src/point_cloud_blender/src/pointCloudBlender.cpp
@@ -18,6 +18,7 @@
 
 using std::placeholders::_1;
 using std::placeholders::_2;
+using namespace std::chrono_literals;
 
 class PointCloudBlender : public rclcpp:: Node{
     public:
@@ -29,44 +30,56 @@ class PointCloudBlender : public rclcpp:: Node{
             pc_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("merge_pc", 10);
             
             // Init sub
-            bool_sub_.subscribe(this, "motion_bool");
-            pc_sub_.subscribe(this, "camera/camera/depth/color/points");
+            bool_sub_ = this->create_subscription<std_msgs::msg::Header>("motion_bool", 10, std::bind(&PointCloudBlender::updateFlag, this, _1));
+            pc_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("camera/camera/depth/color/points", 10, std::bind(&PointCloudBlender::updatePCL, this, _1));
 
-            sync_ = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(
-                SyncPolicy(10), bool_sub_, pc_sub_);
-
-            // Register callback
-            sync_->registerCallback(std::bind(&PointCloudBlender::confirmAngle, this, _1, _2));
+            // Init timer
+            timer_ = this->create_wall_timer(200ms, std::bind(&PointCloudBlender::publishCloud, this));
 
             // Init direction, angle and point cloud
             dirFlag = 1;
             angle = 0;
+            captFlag = false;
+            memoryCloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
 
-            // Init publish
+
+            // Init publish angle to force rotation there
             this->publishAngle();
         }
 
-        void confirmAngle(const std_msgs::msg::Header::ConstSharedPtr &bool_msg,  
-                          const sensor_msgs::msg::PointCloud2::ConstSharedPtr &pc_msg)
+    private:
+        void updateFlag(const std_msgs::msg::Header::ConstSharedPtr &bool_msg)
         {
-            RCLCPP_INFO(this->get_logger(), "Message Recieved");
+            if (bool_msg->frame_id == "1")
+            {
+                captFlag = true;
+            }
+        }
 
-            if (bool_msg->frame_id == "1") {
+        void updatePCL(const sensor_msgs::msg::PointCloud2::ConstSharedPtr &pc_msg)
+        {
+            if (captFlag) // Means only when we want to take photos will we update the PC
+            {
+                captFlag = false;
+                RCLCPP_INFO(this->get_logger(), "PCL Received");
+                
                 // Get point cloud
-                pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+                pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
                 pcl::fromROSMsg(*pc_msg, *cloud);
-                pcl::PointCloud<pcl::PointXYZ>::Ptr trans_cloud;
+                pcl::PointCloud<pcl::PointXYZ>::Ptr trans_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
 
                 if (memoryCloud->empty()){
-                    Eigen::Matrix4f t_matrix = calcTransform(angle, 0);
+                    RCLCPP_INFO(this->get_logger(), "PC Initialised");
+                    Eigen::Matrix4f t_matrix = this->calcTransform(angle, 0);
                     pcl::transformPointCloud(*cloud, *trans_cloud, t_matrix);
                     // Default as first cloud
                     *memoryCloud = *cloud;
                 }
+
                 else
                 {
                     // Transform new point cloud
-                    Eigen::Matrix4f t_matrix = calcTransform(angle, 30);
+                    Eigen::Matrix4f t_matrix = this->calcTransform(angle, 30);
                     pcl::transformPointCloud(*cloud, *trans_cloud, t_matrix);
 
                     // Add new point cloud to old point cloud
@@ -75,39 +88,36 @@ class PointCloudBlender : public rclcpp:: Node{
                     // Filter cloud
                     pcl::VoxelGrid<pcl::PointXYZ> voxel;
                     voxel.setInputCloud(memoryCloud);
-                    voxel.setLeafSize(0.01f, 0.01f, 0.01f); // 1cm voxels
+                    voxel.setLeafSize(0.05f, 0.05f, 0.05f); // 5cm voxels
 
                     pcl::PointCloud<pcl::PointXYZ>::Ptr filterCloud(new pcl::PointCloud<pcl::PointXYZ>); // temp filtered cloud
                     voxel.filter(*filterCloud);
                     memoryCloud = filterCloud; // Replace with new cloud
-
-                    // Publish point cloud
-                    this->publishCloud();
                 }
-                
-                // Update new angle
+
+                // Update direction of rotation
                 if (angle >= 180){
                     dirFlag = -1;
                 }
                 else if (angle <= 0) {
                     dirFlag = 1;
                 }
-                
+
+                // Update new angle
                 angle = angle + dirFlag * 30;
-                
-                // Publish new angle
+                // Send new angle to Arduino, needs to be blocking
                 this->publishAngle();
             }
-
-        };
-
-    private:
+        }
+    
         void publishAngle()
         {
             auto message = std_msgs::msg::Int32();
             message.data = angle;
             RCLCPP_INFO(this->get_logger(), "Angle Published");
             ang_pub_->publish(message);
+
+            captFlag = true;
         }
 
         void publishCloud()
@@ -152,21 +162,17 @@ class PointCloudBlender : public rclcpp:: Node{
         rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_pub_;
 
         // Subscribers
-        message_filters::Subscriber<std_msgs::msg::Header> bool_sub_;
-        message_filters::Subscriber<sensor_msgs::msg::PointCloud2> pc_sub_;
-
-        // Synchronizer
-        typedef message_filters::sync_policies::ApproximateTime<
-            std_msgs::msg::Header,
-            sensor_msgs::msg::PointCloud2
-        > SyncPolicy;
-
-        std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
+        rclcpp::Subscription<std_msgs::msg::Header>::SharedPtr bool_sub_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pc_sub_;
         
         // Constant
         int angle;
         pcl::PointCloud<pcl::PointXYZ>::Ptr memoryCloud;
         int dirFlag;
+        bool captFlag;
+
+        // Timer
+        rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char *argv[])
