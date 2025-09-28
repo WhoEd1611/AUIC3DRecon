3e0bf1d96a687a41e61d32b9bcdd8e0a446b8791
WhoEd1611 <edriclay2002@outlook.com>
2025-09-28 12:41:55 +1000

    init code for image stitcher

---
+++ src/image_stitcher/image_stitcher/stitch_image.py
@@ -0,0 +1,15 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String, Header, Int32
+from sensor_msgs.msg import Image
+
+class ImageStitcherNode(Node):
+    def __init__(self):
+        super().__init__("image_stitcher")
+
+        # Subscribers
+        self.rgb_sub = self.create_subscription(Image, "camera/camera/aligned_depth_to_color/image_raw") # Reads the depth aligned images
+
+        # Publishers
+        self.stitch_pub = self.create_publisher(Image, "stitched_Image") # Publishes the final stitched image
