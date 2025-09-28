063deeafafc02dbda76cc76edf1eb7027fd00f6c
WhoEd1611 <edriclay2002@outlook.com>
2025-09-28 13:19:32 +1000

    added method such that we can filter out depths in rgb image and set to black

--- src/image_stitcher/image_stitcher/stitch_image.py
+++ src/image_stitcher/image_stitcher/stitch_image.py
@@ -9,9 +9,14 @@ from message_filters import ApproximateTimeSynchronizer, Subscriber
 import cv2
 from cv_bridge import CvBridge
 
+import numpy as np
+
 class ImageStitcherNode(Node):
     def __init__(self):
         super().__init__("image_stitcher")
+
+        # Parameters
+        self.declare_parameter('max_distance', 0.8)
 
         # Subscribers
         # self.rgb_sub = self.create_subscription(Image, "camera/camera/aligned_depth_to_color/image_raw", self.stitchCallback, 10) # Reads the depth aligned images
@@ -27,21 +32,35 @@ class ImageStitcherNode(Node):
         self.stitch_pub = self.create_publisher(Image, "stitched_Image", 10) # Publishes the final stitched image
         
         # Data
-        self.stitched_Image = None
+        self.stitchedImage = None
+        self.maxDist = self.get_parameter('max_distance').get_parameter_value().double_value
 
         # Other
         self.br = CvBridge()
     
     def stitchCallback(self, RGBD_msg: Image, depth_msg: Image):
         self.get_logger().info('Receiving video frame')
-        frame = self.br.imgmsg_to_cv2(RGBD_msg)
+
+        # Extract to cv2
+        rgbFrame = self.br.imgmsg_to_cv2(RGBD_msg)
+        depthFrame = self.br.imgmsg_to_cv2(depth_msg)
+
+        filteredFrame = self.filterImage(rgbFrame, depthFrame)
         
         # Initial frame
-        if not self.stitched_Image: # check if image is empty
-            pass
+        if not self.stitchedImage: # check if image is empty
+            self.stitchedImage = filteredFrame
 
         # Other frame
         else:
             pass
 
-        self.stitch_pub.publish(self.stitched_Image)
+        self.stitch_pub.publish(self.stitchedImage)
+
+    def filterImage(self, RGB_image, depth_image):
+    
+        # Filter using numpy
+        depthRGB = depth_image[:, :, None]
+        filtered = np.where(depthRGB>self.maxDist, RGB_image, [0,0,0])
+
+        return filtered
