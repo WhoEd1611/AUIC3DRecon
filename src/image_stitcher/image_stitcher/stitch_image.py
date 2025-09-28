6b4e589ced25b2bee4eaa7ef0b28a60537b7944c
WhoEd1611 <edriclay2002@outlook.com>
2025-09-28 14:32:51 +1000

    added skeleton to compare frames for difference

--- src/image_stitcher/image_stitcher/stitch_image.py
+++ src/image_stitcher/image_stitcher/stitch_image.py
@@ -25,9 +25,10 @@ class ImageStitcherNode(Node):
 
         self.rgb_sub = Subscriber(Image, "camera/camera/aligned_depth_to_color/image_raw") # Reads the depth aligned images
         self.depth_sub = Subscriber(Image, "camera/camera/depth/image_rect_raw") # Reads the depth images
-        self.cam_sub = self.create_subscription(CameraInfo, "/camera/camera/aligned_depth_to_color/camera_info", self.setStitchedImageSize)
+        self.angle_sub = Subscriber(Int32, "angle") # Reads the current image angle
+        self.cam_sub = self.create_subscription(CameraInfo, "/camera/camera/aligned_depth_to_color/camera_info", self.setStitchedImageSize) # Reads the camera intrinsics
 
-        self.ats = ApproximateTimeSynchronizer([self.rgb_sub, self.depth_sub], 10, 0.1)
+        self.ats = ApproximateTimeSynchronizer([self.rgb_sub, self.depth_sub, self.angle_sub], 10, 0.1)
         self.ats.registerCallback(self.stitchCallback)
 
         # Publishers
@@ -68,47 +69,42 @@ class ImageStitcherNode(Node):
         
         del self.cam_sub
     
-    def stitchCallback(self, RGBD_msg: Image, depth_msg: Image):
+    def stitchCallback(self, RGBD_msg: Image, depth_msg: Image, angle_msg: Int32):
         self.get_logger().info('Receiving video frame')
 
         status = False # Used to prevent failed stitches from being published
-        
 
         # Extract to cv2
         rgbFrame = self.br.imgmsg_to_cv2(RGBD_msg)
         depthFrame = self.br.imgmsg_to_cv2(depth_msg)
 
+        # Get angle data
+        angle = angle_msg.data
+
         filteredFrame = self.filterImage(rgbFrame, depthFrame)
+        imgs = [self.stitchedImage, filteredFrame]
         
-        # Initial frame
-        if not self.stitchedImage: # check if image is empty
-            status = True
-            self.stitchedImage = filteredFrame
+        # Checks to see if anything has changed in the image to see if need to update pcl
+        notReplace = self.compareFrame(filteredFrame, angle) # Compares the captured image with the current frame
 
-        # Other frame
+        # Doesn't need to replace image + pcl
+        if notReplace:
+            pass
+
+        # Does need to replace image + PCL
         else:
-            imgs = [self.stitchedImage, filteredFrame]
-            
-            notReplace = True # Two conditions: need to replace or not?
-
-            # Doesn't need to replace image + pcl
-            if notReplace:
-                pass
-
-            # Does need to replace image + PCL
+            status, stitchedImage = self.stitcher.stitch(imgs)
+            if status:
+                self.stitchedImage = stitchedImage
+                
+                # Tell PCL to update point cloud
+                update_msg = JointState()
+                update_msg.name = ["angle"]
+                # update_msg.position = [angle] # Need to tell where to update PCL
+                update_msg.effort = [1] # Used to tell ICP blender to update PCL
+                self.update_pub.publish(update_msg)
             else:
-                status, stitchedImage = self.stitcher.stitch(imgs)
-                if status:
-                    self.stitchedImage = stitchedImage
-                    
-                    # Tell PCL to update point cloud
-                    update_msg = JointState()
-                    update_msg.name = ["angle"]
-                    # update_msg.position = [angle] # Need to tell where to update PCL
-                    update_msg.effort = [1] # Used to tell ICP blender to update PCL
-                    self.update_pub.publish(update_msg)
-                else:
-                    self.get_logger().info('Issue with image stitching')
+                self.get_logger().info('Issue with image stitching')
 
         # Always publish stitched image
         self.stitch_pub.publish(self.stitchedImage)
@@ -119,3 +115,11 @@ class ImageStitcherNode(Node):
         filtered = np.where(depthRGB>self.maxDist, RGB_image, [0,0,0])
 
         return filtered
+    
+    def compareFrame(self, image_frame, angle):
+        # Step 1: Extract from self.stitchedImage equivalent image
+
+        # Step 2: Compare two images
+
+        diff = False
+        return diff
